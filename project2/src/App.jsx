c19326290d757bacfaf30ede08f2a5574c8fc155
Angel <aavila002@citymail.cuny.edu>
2023-03-06 23:48:10 -0500

    images added to cards

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -9,46 +9,53 @@ const App = () => {
   const flashcards = [
     {
         "title": "Array",
-        "description": "In an array, elements in memory are arranged in continuous memory. All the elements of an array are of the same type. And, the type of elements that can be stored in the form of arrays is determined by the programming language. (List in Python)"
+        "description": "In an array, elements in memory are arranged in continuous memory. All the elements of an array are of the same type. And, the type of elements that can be stored in the form of arrays is determined by the programming language. (List in Python)",
+        "image": "./src/assets/array.png"
     },
     {
         "title": "Tuple",
-        "description": "Tuples are the same as lists are with the exception that the data once entered into the tuple cannot be changed no matter what. The only exception is when the data inside the tuple is mutable, only then the tuple data can be changed. "
+        "description": "Tuples are the same as lists are with the exception that the data once entered into the tuple cannot be changed no matter what. The only exception is when the data inside the tuple is mutable, only then the tuple data can be changed. ",
+        "image": "./src/assets/tuple.png"
     },
     {
         "title": "Stack",
-        "description": "In stack data structure, elements are stored in the LIFO principle. That is, the last element stored in a stack will be removed first."
+        "description": "In stack data structure, elements are stored in the LIFO principle. That is, the last element stored in a stack will be removed first.",
+        "image": "./src/assets/stack.png"
     },
     {
         "title": "Queue",
-        "description": "Unlike stack, the queue data structure works in the FIFO principle where first element stored in the queue will be removed first."
+        "description": "Unlike stack, the queue data structure works in the FIFO principle where first element stored in the queue will be removed first.",
+        "image": "./src/assets/queue.png"
     },
     {
         "title": "String",
-        "description": "String is a collection of alphabets, words or other characters. "
+        "description": "String is a collection of alphabets, words or other characters. ",
+        "image": "./src/assets/string.png"
     },
     {
         "title": "Hashmap",
-        "description": "Hash maps are a common data structure used to store key-value pairs for efficient retrieval. A value stored in a hash map is retrieved using the key under which it was stored. (Dictionary in Python)"
+        "description": "Hash maps are a common data structure used to store key-value pairs for efficient retrieval. A value stored in a hash map is retrieved using the key under which it was stored. (Dictionary in Python)",
+        "image": "./src/assets/hashmap.png"
     },
     {
         "title": "Set",
-        "description": "Sets are a collection of unordered elements that are unique. It is a collection which is unordered, unchangeable*, and unindexed."
+        "description": "Sets are a collection of unordered elements that are unique. It is a collection which is unordered, unchangeable*, and unindexed.",
+        "image": "./src/assets/set.png"
     },
     {
         "title": "Linked List",
-        "description": "In linked list data structure, data elements are connected through a series of nodes. And, each node contains the data items and address to the next node."
+        "description": "In linked list data structure, data elements are connected through a series of nodes. And, each node contains the data items and address to the next node.",
+        "image": "./src/assets/linkedlist.png"
     },
     {
         "title": "Tree",
-        "description": "Similar to a graph, a tree is also a collection of vertices and edges. However, in tree data structure, there can only be one edge between two vertices."
+        "description": "Similar to a graph, a tree is also a collection of vertices and edges. However, in tree data structure, there can only be one edge between two vertices.",
+        "image": "./src/assets/tree.png"
     },
     {
         "title": "Graph",
-        "description": "In graph data structure, each node is called vertex and each vertex is connected to other vertices through edges."
-    },
-    {
-      "title": "End",
+        "description": "In graph data structure, each node is called vertex and each vertex is connected to other vertices through edges.",
+        "image": "./src/assets/graph.png"
     }
   ]
 
@@ -87,7 +94,8 @@ const App = () => {
                 </div>
                 
                 <div className="backCard">
-                    <h2>{flashcards[index].description}</h2>
+                    <p>{flashcards[index].description}</p>
+                    <img src={flashcards[index].image} width="80%"/>
                 </div>
             </div>
         </div>
